fd5894ce033a8268cab30cc6b25fa49ade033a9e
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-02-07 19:19:18 +0100

    Refactor: Use Enum and match to avoid repetition

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,13 @@ impl FileInfo {
     }
 }
 
+enum SearchType {
+    Directory,
+    File,
+    Editable,
+    Other,
+}
+
 impl PartialEq for FileInfo {
     fn eq(&self, other: &Self) -> bool {
         self.filename == other.filename
@@ -95,41 +102,46 @@ fn main() {
         }
     }
 
+    let searched_for = if called_by_name.contains("dir") {
+        SearchType::Directory
+    } else if called_by_name.contains("file") {
+        SearchType::File
+    } else if called_by_name.contains("editable") {
+        SearchType::Editable
+    } else {
+        SearchType::Other
+    };
+
     while let Some(orf) = files.peek() {
         if !Path::new(&orf.1.filename).exists() {
             files.pop();
             continue;
         }
-        if called_by_name.contains("dir"){
-            if fs::metadata(&orf.1.filename).unwrap().is_dir() {
-                Command::new(&prog).arg(&orf.1.filename).spawn().expect("Failed to execute program with relevant file");
-                break;
-            } else {
-                files.pop();
-                continue;
+        match searched_for {
+            SearchType::Directory => {
+                if !fs::metadata(&orf.1.filename).unwrap().is_dir() {
+                    files.pop();
+                    continue;
+                }
             }
-        } else if called_by_name.contains("file") {
-            if fs::metadata(&orf.1.filename).unwrap().is_file() {
-                Command::new(&prog).arg(&orf.1.filename).spawn().expect("Failed to execute program with relevant file");
-                break;
-            } else {
-                files.pop();
-                continue;
+            SearchType::File => {
+                if !fs::metadata(&orf.1.filename).unwrap().is_file() {
+                    files.pop();
+                    continue;
+                }
             }
-        } else if called_by_name.contains("editable") {
-            let md = fs::metadata(&orf.1.filename).unwrap();
-            const EDITABLE_FILE_MAX_SIZE_CRITERION : u64= 10 * 1000; //10 kilos
-            if md.is_file() && !md.permissions().readonly() && md.len() <  EDITABLE_FILE_MAX_SIZE_CRITERION { //NOTE: this criterion is ok for now. later we must find some features like "bigram patterns" of first 100 characters... that is not as time-consuming as a `$(file thisfile)` invocation
-                Command::new(&prog).arg(&orf.1.filename).spawn().expect("Failed to execute program with relevant file");
-                break;
-            } else {
-                files.pop();
-                continue;
+            SearchType::Editable => {
+                let md = fs::metadata(&orf.1.filename).unwrap();
+                const EDITABLE_FILE_MAX_SIZE_CRITERION : u64= 10 * 1000; //10 kilos
+                if !(md.is_file() && !md.permissions().readonly() && md.len() <  EDITABLE_FILE_MAX_SIZE_CRITERION) { //NOTE: this criterion is ok for now. later we must find some features like "bigram patterns" of first 100 characters... that is not as time-consuming as a `$(file thisfile)` invocation
+                    files.pop();
+                    continue;
+                }
             }
-        } else {
-                Command::new(&prog).arg(&orf.1.filename).spawn().expect("Failed to execute program with relevant file");
-                break;
+            _ => ()
         }
+        Command::new(&prog).arg(&orf.1.filename).spawn().expect("Failed to execute program with relevant file");
+        break;
     }
     // Command::new("fasd").arg("-A").arg(&orf.filename).spawn().expect("Failed to add to fasd");
 }
